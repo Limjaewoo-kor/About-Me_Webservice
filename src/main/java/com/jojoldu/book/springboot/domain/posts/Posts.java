85b5b771531ca1a923d95b30c52a8544ab2cdee8
Limjaewoo <ljw3944@naver.com>
2020-11-03 20:53:57 +0900

    11/3

--- src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
@@ -9,7 +9,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,5 +28,10 @@ public class Posts {
         this.title = title;
         this.content = content;
         this.author = author;
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
     }
 }
--- src/test/java/com/jojoldu/book/springboot/web/dto/HelloResponseDtoTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/dto/HelloResponseDtoTest.java
@@ -1,6 +1,5 @@
-package com.jojoldu.book.springboot.dto;
+package com.jojoldu.book.springboot.web.dto;
 
-import com.jojoldu.book.springboot.web.dto.HelloResponseDto;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
