6d1caa4451c6904ccb0c6ea4fb286ac1e8f24893
Limjaewoo <ljw3944@naver.com>
2020-11-10 00:12:00 +0900

    11/9 clean

--- src/test/java/com/jojoldu/book/springboot/web/IndexControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/IndexControllerTest.java
@@ -21,8 +21,7 @@ public class IndexControllerTest {
     public void 메인페이지_로딩() {
         //when
         String body = this.restTemplate.getForObject("/",String.class);
-
         //then
-        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
+        assertThat(body).contains("로그인");
     }
 }
