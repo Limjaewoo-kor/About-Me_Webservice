41a92461824baacc8f69eee800f7decbba132e3f
Limjaewoo <ljw3944@naver.com>
2020-11-11 11:39:53 +0900

    11 / 11  commit

--- src/test/java/com/jojoldu/book/springboot/web/IndexControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/IndexControllerTest.java
@@ -24,4 +24,40 @@ public class IndexControllerTest {
         //then
         assertThat(body).contains("로그인");
     }
+
+    @Test
+    public void who페이지_로딩() {
+        //when
+        String body = this.restTemplate.getForObject("/who",String.class);
+        //then
+        assertThat(body).contains("Lim Jae Woo 는?");
+    }
+    @Test
+    public void project페이지_로딩() {
+        //when
+        String body = this.restTemplate.getForObject("/project",String.class);
+        //then
+        assertThat(body).contains("주차장 금액 정산 프로젝트는");
+    }
+    @Test
+    public void project2페이지_로딩() {
+        //when
+        String body = this.restTemplate.getForObject("/project2",String.class);
+        //then
+        assertThat(body).contains("숨은 카페 찾기 프로젝트는");
+    }
+    @Test
+    public void project3페이지_로딩() {
+        //when
+        String body = this.restTemplate.getForObject("/project3",String.class);
+        //then
+        assertThat(body).contains("자기 소개 프로젝트는");
+    }
+    @Test
+    public void board페이지_로딩() {
+        //when
+        String body = this.restTemplate.getForObject("/board",String.class);
+        //then
+        assertThat(body).contains("글 등록");
+    }
 }
